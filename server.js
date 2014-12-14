8770238ced5385994a8cbc31fc2a038a34cb6ab0
Clement Bourgoin <cb@nokto.net>
2014-12-14 18:34:33 +0100

    Use static to deliver client files

--- server.js
+++ server.js
@@ -1,22 +1,6 @@
 var express = require('express');
 var app = express();
-
-app.get('/', function(req, res){
-  	res.sendFile(__dirname+'/client/index.html');
-});
-
-app.get('/noktime.css', function(req, res){
-  	res.sendFile(__dirname+'/client/noktime.css');
-});
-
-app.get('/noktime.js', function(req, res){
-  	res.sendFile(__dirname+'/client/noktime.js');
-});
-
-app.get('/favicon.png', function(req, res){
-  	res.sendFile(__dirname+'/client/favicon.png');
-});
-
+app.use(express.static(__dirname + '/client'));
 
 var server = app.listen(8080, function() {
     console.log('Listening on port %d', server.address().port);
